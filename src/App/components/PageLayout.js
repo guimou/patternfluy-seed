9108a421d7e39354f2f0b96e9860986a3db128a5
Unknown <guillaume.moutier@gmail.com>
2019-05-19 17:41:39 -0400

    Basic App

---
+++ src/App/components/PageLayout.js
@@ -0,0 +1,197 @@
+import React from 'react';
+import {
+    Avatar,
+    Brand,
+    Button,
+    ButtonVariant,
+    Card,
+    CardBody,
+    Dropdown,
+    DropdownToggle,
+    DropdownItem,
+    DropdownSeparator,
+    Gallery,
+    GalleryItem,
+    KebabToggle,
+    Nav,
+    NavItem,
+    NavList,
+    NavVariants,
+    Page,
+    PageHeader,
+    PageSection,
+    PageSectionVariants,
+    PageSidebar,
+    SkipToContent,
+    TextContent,
+    Text,
+    Toolbar,
+    ToolbarGroup,
+    ToolbarItem
+  } from '@patternfly/react-core';
+  // make sure you've installed @patternfly/patternfly
+  import { css } from '@patternfly/react-styles';
+  import { BellIcon, CogIcon } from '@patternfly/react-icons';
+  
+  class PageLayout extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isDropdownOpen: false,
+        isKebabDropdownOpen: false,
+        activeItem: 0
+      };
+    }
+  
+    onDropdownToggle = isDropdownOpen => {
+      this.setState({
+        isDropdownOpen
+      });
+    };
+  
+    onDropdownSelect = event => {
+      this.setState({
+        isDropdownOpen: !this.state.isDropdownOpen
+      });
+    };
+  
+    onKebabDropdownToggle = isKebabDropdownOpen => {
+      this.setState({
+        isKebabDropdownOpen
+      });
+    };
+  
+    onKebabDropdownSelect = event => {
+      this.setState({
+        isKebabDropdownOpen: !this.state.isKebabDropdownOpen
+      });
+    };
+  
+    onNavSelect = result => {
+      this.setState({
+        activeItem: result.itemId
+      });
+    };
+  
+    render() {
+      const { isDropdownOpen, isKebabDropdownOpen, activeItem } = this.state;
+  
+      const PageNav = (
+        <Nav onSelect={this.onNavSelect} aria-label="Nav">
+          <NavList variant={NavVariants.simple}>
+            <NavItem to="#nav-link1" itemId={0} isActive={activeItem === 0}>
+              System Panel
+            </NavItem>
+            <NavItem to="#nav-link2" itemId={1} isActive={activeItem === 1}>
+              Policy
+            </NavItem>
+            <NavItem to="#nav-link3" itemId={2} isActive={activeItem === 2}>
+              Authentication
+            </NavItem>
+            <NavItem to="#nav-link4" itemId={3} isActive={activeItem === 3}>
+              Network Services
+            </NavItem>
+            <NavItem to="#nav-link5" itemId={4} isActive={activeItem === 4}>
+              Server
+            </NavItem>
+          </NavList>
+        </Nav>
+      );
+      const kebabDropdownItems = [
+        <DropdownItem>
+          <BellIcon /> Notifications
+        </DropdownItem>,
+        <DropdownItem>
+          <CogIcon /> Settings
+        </DropdownItem>
+      ];
+      const userDropdownItems = [
+        <DropdownItem>Link</DropdownItem>,
+        <DropdownItem component="button">Action</DropdownItem>,
+        <DropdownItem isDisabled>Disabled Link</DropdownItem>,
+        <DropdownItem isDisabled component="button">
+          Disabled Action
+        </DropdownItem>,
+        <DropdownSeparator />,
+        <DropdownItem>Separated Link</DropdownItem>,
+        <DropdownItem component="button">Separated Action</DropdownItem>
+      ];
+      const PageToolbar = (
+        <Toolbar>
+          <ToolbarGroup>
+            <ToolbarItem>
+              <Button id="simple-example-uid-01" aria-label="Notifications actions" variant={ButtonVariant.plain}>
+                <BellIcon />
+              </Button>
+            </ToolbarItem>
+            <ToolbarItem>
+              <Button id="simple-example-uid-02" aria-label="Settings actions" variant={ButtonVariant.plain}>
+                <CogIcon />
+              </Button>
+            </ToolbarItem>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <ToolbarItem>
+              <Dropdown
+                isPlain
+                position="right"
+                onSelect={this.onKebabDropdownSelect}
+                toggle={<KebabToggle onToggle={this.onKebabDropdownToggle} />}
+                isOpen={isKebabDropdownOpen}
+                dropdownItems={kebabDropdownItems}
+              />
+            </ToolbarItem>
+            <ToolbarItem>
+              <Dropdown
+                isPlain
+                position="right"
+                onSelect={this.onDropdownSelect}
+                isOpen={isDropdownOpen}
+                toggle={<DropdownToggle onToggle={this.onDropdownToggle}>Kyle Baker</DropdownToggle>}
+                dropdownItems={userDropdownItems}
+              />
+            </ToolbarItem>
+          </ToolbarGroup>
+        </Toolbar>
+      );
+      const Header = (
+        <PageHeader
+          toolbar={PageToolbar}
+          showNavToggle
+        />
+      );
+      const Sidebar = <PageSidebar nav={PageNav} />;
+      const PageSkipToContent = (
+        <SkipToContent href="#main-content-page-layout-default-nav">Skip to Content</SkipToContent>
+      );
+  
+      return (
+        <React.Fragment>
+          <Page header={Header} sidebar={Sidebar} isManagedSidebar skipToContent={PageSkipToContent}>
+            <PageSection variant={PageSectionVariants.light}>
+              <TextContent>
+                <Text component="h1">Main Title</Text>
+                <Text component="p">
+                  Body text should be Overpass Regular at 16px. It should have leading of 24px because <br />
+                  of it’s relative line height of 1.5.
+                </Text>
+              </TextContent>
+            </PageSection>
+            <PageSection>
+              <Gallery gutter="md">
+                {Array.apply(0, Array(10)).map((x, i) => (
+                  <GalleryItem key={i}>
+                    <Card>
+                      <CardBody>This is a card</CardBody>
+                    </Card>
+                  </GalleryItem>
+                ))}
+              </Gallery>
+            </PageSection>
+          </Page>
+        </React.Fragment>
+      );
+    }
+  }
+  
+  export { PageLayout };
